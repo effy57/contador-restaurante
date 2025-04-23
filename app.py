210e8e2af8321241428fc1016052e293d6783d2e
effy57 <stefaniioso6@gmail.com>
2025-04-23 08:00:49 -0500

    Move app.py to the root of the project

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def finalizar():
     respuesta = "\n".join([f"Min {i+1}: {c}" for i, c in enumerate(conteo_por_minuto)])
     return f"Consolidado por minuto:\n{respuesta}", 200
 
+
 if _name_ == '_main_':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
